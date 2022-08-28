2b4e0285275298bde6a6ef3621c92eab36dd8880
r20de20 <werykintosz@wp.eu>
2022-08-28 16:18:57 +0200

    Added variable names for regionsContent

---
+++ src/RegionsContent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRScenarioCreatorEnhanced.src
+{
+    public class RegionsContent
+    {
+        string regionName;
+        string regionPrefix;
+        string color;
+        int blockNumber;
+        int continentNumber;
+        int flagNumber;
+        int musicNumber;
+        int electionDate; // yyyymmddd
+        int politicalLeaning;
+        int governmentType;
+        string armsAvailability;
+        string worldAvailability;
+        float population;
+        float armyPopulation;
+        float minimumReserve;
+        float GDPC;
+        float debt;
+        float treasury;
+        double inflation;
+        double treatyIntegrity;
+        double worldIntegrity;
+        double creditRating;
+        int defConLevel;
+        double environmentalRating;
+        double literacyLevel;
+        double fanaticism;
+        double loyalty;
+        double unemployment;
+        double crimeRate;
+        double tourismRating;
+        double lifeExpectancy;
+        double averageChildren;
+        double militarySibsidy;
+        int altBlockNumber;
+        int techLevel;
+        double civilianApproval;
+        double revoltPossibility;
+        double militaryApproval;
+        double militarySalary;
+        double militaryIntel;
+        string racePrimary; // combo
+        string raceSecondary;
+        string sphere;
+        string forcesPlan;
+        string agenda;
+        string aiStance;
+        int parentRegionID;
+        double alertLevel;
+        double efficiency;
+        double coupPossibility;
+        double buyingPower;
+        double militaryMaintenance;
+        double militaryResearch;
+        string altName;
+        int capitalX;
+        int capitalY;
+        bool nonPlayable;
+    }
+}
