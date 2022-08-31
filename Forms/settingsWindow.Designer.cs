2f882ebb349dc4c9e9039ac2554d690feb74df2e
r20de20 <werykintosz@wp.eu>
2022-08-31 21:45:18 +0200

    Updated settings window design

--- Forms/settingsWindow.Designer.cs
+++ Forms/settingsWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(settingsWindow));
             this.label1 = new System.Windows.Forms.Label();
             this.trackScale = new System.Windows.Forms.TrackBar();
             this.label2 = new System.Windows.Forms.Label();
@@ -40,16 +41,17 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 70);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.Size = new System.Drawing.Size(152, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "Editor Scale";
             // 
             // trackScale
             // 
             this.trackScale.LargeChange = 1;
-            this.trackScale.Location = new System.Drawing.Point(123, 48);
+            this.trackScale.Location = new System.Drawing.Point(170, 47);
             this.trackScale.Maximum = 2;
             this.trackScale.Name = "trackScale";
             this.trackScale.Size = new System.Drawing.Size(295, 69);
@@ -60,35 +62,39 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(368, 120);
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(415, 119);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.Size = new System.Drawing.Size(71, 30);
             this.label2.TabIndex = 2;
             this.label2.Text = "100%";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(252, 120);
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(299, 119);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.Size = new System.Drawing.Size(58, 30);
             this.label3.TabIndex = 3;
             this.label3.Text = "75%";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(119, 120);
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(166, 119);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 20);
+            this.label4.Size = new System.Drawing.Size(58, 30);
             this.label4.TabIndex = 4;
             this.label4.Text = "50%";
             // 
             // applyScaleButton
             // 
-            this.applyScaleButton.Location = new System.Drawing.Point(424, 48);
+            this.applyScaleButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.applyScaleButton.Location = new System.Drawing.Point(499, 45);
             this.applyScaleButton.Name = "applyScaleButton";
-            this.applyScaleButton.Size = new System.Drawing.Size(151, 65);
+            this.applyScaleButton.Size = new System.Drawing.Size(198, 69);
             this.applyScaleButton.TabIndex = 5;
             this.applyScaleButton.Text = "Apply Scale";
             this.applyScaleButton.UseVisualStyleBackColor = true;
@@ -98,20 +104,22 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(598, 544);
+            this.ClientSize = new System.Drawing.Size(709, 212);
             this.Controls.Add(this.applyScaleButton);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.trackScale);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "settingsWindow";
             this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "settingsWindow";
+            this.Text = "Editor Settings";
             ((System.ComponentModel.ISupportInitialize)(this.trackScale)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
